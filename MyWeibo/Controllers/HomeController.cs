2af5d298820bec4122fde8dccc6e925e1c7b3fca
halcyons <halcyonsong@gmail.com>
2013-05-15 08:54:49 -0700

    2013-5-15 添加首页显示最近活跃用户功能

--- MyWeibo/Controllers/HomeController.cs
+++ MyWeibo/Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MyWeibo.ViewModels;
+using MyWeibo.BLL;
 namespace MyWeibo.Controllers
 {
     public class HomeController : Controller
     {
+        HomeBLL homeBLL = new HomeBLL();   
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            HomeViewModel model = new HomeViewModel();
+            model.users = homeBLL.GetHomeUserInfo(10);
 
-            return View();
+            return View(model);
         }
 
         public ActionResult About()
